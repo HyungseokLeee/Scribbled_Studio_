8976b89a736b25924f66653da0130954ae8a7f49
Username909 <43253195+Username909@users.noreply.github.com>
2020-12-01 13:22:16 -0500

    Added a ranged enemy

---
+++ Assets/Scripts/EnemyBullet.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    public float moveSpeed = 7f;
+
+    private Rigidbody2D rb;
+
+    public Transform target;
+
+    private Vector2 moveDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        moveDirection = (target.transform.position - transform.position).normalized * moveSpeed;
+        rb.velocity = new Vector2(moveDirection.x, moveDirection.y);
+        Destroy(gameObject, 3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag.Equals("Player"))
+        {
+            Debug.Log("Hit");
+            Destroy(gameObject);
+        }
+        if (col.gameObject.tag.Equals("Wall"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/RangedEnemy.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangedEnemy : MonoBehaviour
+{
+    public GameObject bullet;
+    public float speed;
+    public Transform target;
+    private GameController gC;
+    public int lives = 5;
+    public float fireRate;
+    private float nextFire;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject gCO = GameObject.FindWithTag("GameController");
+        gC = gCO.GetComponent<GameController>();
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        nextFire = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, target.position) > 1.5)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+
+        if (lives < 1)
+        {
+            Destroy(this.gameObject);
+            gC.Score += 100;
+        }
+        CheckIfTimeToFire();
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Bullet")
+        {
+            lives = lives - 1;
+            Debug.Log("SE");
+            Destroy(other.gameObject);
+        }
+    }
+
+    void CheckIfTimeToFire()
+    {
+        if (Time.time > nextFire)
+        {
+            Instantiate(bullet, transform.position, Quaternion.identity);
+            nextFire = Time.time + fireRate;
+        }
+    }
+}
+
