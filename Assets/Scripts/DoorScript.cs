8f15c17ae35b4bffbded7a9b4db07b87bb04ca41
XeonDigital <xinrongisaweosm2@gmail.com>
2020-12-03 15:33:41 -0500

    added scene transition to the next level

---
+++ Assets/Scripts/DoorScript.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorScript : MonoBehaviour
+{
+	public String sceneName;
+
+	public GameObject player;
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.tag == player.tag)
+		{
+			SceneManager.LoadScene(sceneName);
+		}
+	}
+
+}
